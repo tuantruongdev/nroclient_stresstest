492e2a974bce64708750a8af7987daa6e4631ddf
truongntPC <tuantruong.a8k29@gmail.com>
2023-12-15 02:02:53 +0700

    bot

--- src/Main.java
+++ src/Main.java
@@ -15,12 +15,12 @@ import java.util.Random;
 import java.util.logging.Logger;
 
 public class Main {
-//    String serverAddress = "127.0.0.1";
-//
-//    int serverPort = 14445;
-    String serverAddress = "ipkyuccz.ngocrongkyuc.com";
+    String serverAddress = "127.0.0.1";
 
-    int serverPort = 11224;
+    int serverPort = 14445;
+//    String serverAddress = "103.170.121.55";
+//
+//    int serverPort = 11223;
     List<Account> accounts = new ArrayList<>();
 
     //        String serverAddress = "ipkyuccz.ngocrongkyuc.com";
@@ -28,27 +28,25 @@ public class Main {
     public static void main(String[] args) throws SQLException, InterruptedException {
         SQLManager.create(Setting.DB_HOST, 3306, Setting.DB_DATE, Setting.DB_USER, Setting.DB_PASS);
         Main m = new Main();
-        m.loadAccounts();
-        m.loginAllZombies();
-
-        // m.loginZombie("tuantruong", "truongtn1");
-
+        //m.loadAccounts();
+        //m.loginAllZombies();
+        m.loginZombie("tuantruong", "truongtn1");
     }
 
     void loginAllZombies() throws InterruptedException {
         for (Account a : accounts){
             Thread thread = new Thread(() -> loginZombie(a.username,a.password));
             thread.start();
-            Thread.sleep(400);
+            Thread.sleep(300);
         }
     }
 
 
     void loginZombie(String username, String pass) {
         try {
-            if (username.equals("admin")){
-                return;
-            }
+//            if (username.equals("admin")||username.equals("tuantruong")){
+//                return;
+//            }
             Socket socket = new Socket(serverAddress, serverPort);
             Session session = new Session();
             session.add(socket, new IMessageHandler() {
@@ -87,7 +85,111 @@ public class Main {
             msg.writer().flush();
             session.sendMessage(msg);
             int i = 0;
-            while (true) {
+
+//            msg = new Message(-40);
+//            msg.writer().writeByte(1);
+//            msg.writer().writeByte(0);
+//            msg.writer().flush();
+//            session.sendMessage(msg);
+/*            Thread.sleep(20000);
+            msg = new Message(-86);//gd
+            msg.writer().writeByte(1);//accept
+            msg.writer().writeInt(1007279);
+            msg.writer().flush();
+            session.sendMessage(msg);
+            System.out.println("done acp");
+
+            //add item
+            Thread.sleep(2000);
+            msg = new Message(-86);
+            msg.writer().writeByte(2);//add item
+            msg.writer().writeByte(0);//item index
+            msg.writer().writeInt(500);//item quantity
+            msg.writer().flush();
+            session.sendMessage(msg);
+            System.out.println("done add item");
+            Thread.sleep(2000);
+
+
+            //lock item
+            Thread.sleep(2000);
+            msg = new Message(-86);
+            msg.writer().writeByte(5);//lock item
+            msg.writer().flush();
+            session.sendMessage(msg);
+
+            //cat item index
+            Thread.sleep(1000);
+            msg = new Message(-40);
+            msg.writer().writeByte(1);
+            msg.writer().writeByte(0);
+            msg.writer().flush();
+            session.sendMessage(msg);
+
+            //accept  trade
+            Thread.sleep(10000);
+            msg = new Message(-86);
+            msg.writer().writeByte(7);//accept trade
+            msg.writer().flush();
+            session.sendMessage(msg);*/
+
+            //open ba hat mit
+            Thread.sleep(1000);
+            msg = new Message(56);
+            msg.writer().writeShort(21);
+            msg.writer().flush();
+            session.sendMessage(msg);
+
+            //select pha le hoa
+            Thread.sleep(1000);
+            msg = new Message(32);
+            msg.writer().writeShort(56);//npc id
+            msg.writer().writeByte(3);//select thu 4
+            msg.writer().flush();
+            session.sendMessage(msg);
+
+//            //select bang ngoc
+//            Thread.sleep(1000);
+//            msg = new Message(32);
+//            msg.writer().writeShort(21);//npc id
+//            msg.writer().writeByte(0);//select o dau tien
+//            msg.writer().flush();
+//            session.sendMessage(msg);
+
+            //select item
+            Thread.sleep(1000);
+            msg = new Message(-81);
+            msg.writer().writeByte(1);//action
+            msg.writer().writeByte(1);//so luong item
+            msg.writer().writeByte(0);//index item, quan kaio
+            msg.writer().flush();
+            session.sendMessage(msg);
+
+
+
+            //cat item index
+            Thread.sleep(1000);
+            msg = new Message(-40);
+            msg.writer().writeByte(1);
+            msg.writer().writeByte(0);
+            msg.writer().flush();
+            session.sendMessage(msg);
+
+            //upgrade item
+            Thread.sleep(1000);
+            msg = new Message(32);
+            msg.writer().writeShort(5);//npc id
+            msg.writer().writeByte(0);//select x100
+            msg.writer().flush();
+            session.sendMessage(msg);
+
+            Util.log("done all");
+
+
+           while (true) {
+               if (true){
+                   return;
+               }
                 i++;
                 msg = new Message(44);
                 byte[] array = new byte[15];
@@ -106,6 +208,7 @@ public class Main {
                 session.sendMessage(msg);
             }
 
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -114,7 +217,7 @@ public class Main {
     void loadAccounts() throws SQLException {
         Connection conn = SQLManager.getConnection2(Setting.DB_NICK);
         ResultSet rs = null;
-        rs = conn.createStatement().executeQuery("SELECT `username`,`password` FROM `account`  LIMIT 800");
+        rs = conn.createStatement().executeQuery("SELECT `username`,`password` FROM `account`  LIMIT 1500");
         if (rs != null) {
             while (rs.next()) {
                 String username = rs.getString("username");
